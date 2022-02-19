c3f3f6223c8a43eb21163f12864ea6393147daa6
Kanisk <57884127+chakrakan@users.noreply.github.com>
2022-02-18 22:25:55 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  plugins: [new ESLintPlugin({ extensions: ['.js', '.jsx'] })],
+  // https://eslint.org/docs/developer-guide/nodejs-api#-new-eslintoptions
+  plugins: [new ESLintPlugin({ extensions: ['.js', '.jsx'] })], // ESLintPlugin takes in an eslint options object
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
